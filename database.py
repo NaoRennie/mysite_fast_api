a2ca255d3f7c9ee9dd434b9e3b3a9f3766be0900
NaoRennie <naomi.lily.405@gmail.com>
2023-02-16 13:49:44 +0900

    added created post end point

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 from decouple import config
+from typing import Union
 import motor.motor_asyncio
 
 MONGO_API_KEY = config('MONGO_API_KEY')
@@ -9,6 +10,17 @@ database = client.MYSITE_DB
 collection_user = database.user
 collection_todo= database.todo
 
+def todo_serializer(todo) -> dict:
+    return {
+        "id": str(todo["_id"]),
+        "title": todo["title"],
+        "description": todo["description"]
+    }
 
-async def db_create_todo(data: dict) -> dict:
+
+async def db_create_todo(data: dict) -> Union[dict, bool]:
     todo = await collection_todo.insert_one(data)
+    new_todo = await collection_todo.find_one({"_id": todo.inserted_id})
+    if new_todo:
+        return todo_serializer(new_todo)
+    return False
