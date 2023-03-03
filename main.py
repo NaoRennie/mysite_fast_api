91d7272383f26c93a5702fece9d8c7de1c0c4962
NaoRennie <naomi.lily.405@gmail.com>
2023-03-03 14:29:58 +0900

    added origins

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from fastapi_csrf_protect.exceptions import CsrfProtectError
 app = FastAPI()
 app.include_router(route_todo.router)
 app.include_router(route_auth.router)
-origins = ['http://localhost:3000']
+origins = ['http://localhost:3000', 'http://localhost:3000/todo']
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
